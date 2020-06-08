4fc1ad1abb5589ff3ea7e08ba8c4b04bae0e91d8
saci zakaria <sacideveloppeur@gmail.com>
2020-06-08 11:08:55 +0200

    PatientRestController.java modif Import

--- src/main/java/org/devsaci/springmvc/web/PatientRestController.java
+++ src/main/java/org/devsaci/springmvc/web/PatientRestController.java
@@ -1,23 +1,13 @@
 package org.devsaci.springmvc.web;
 
 import java.util.List;
-
-import javax.validation.Valid;
-
 import org.devsaci.springmvc.dao.PatientRepository;
 import org.devsaci.springmvc.entities.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @author Zakaria
